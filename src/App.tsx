73b7e25448dcf9087457ab80f38dfdf0aa9ffe03
Tania Omelko <tanyaomelko39@gmail.com>
2024-04-27 00:28:26 +0300

    rework router to avoid 404 page on gh problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import './App.scss';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import { AuthProvider } from './firebase/authContext';
-import { Header } from './components/Header/Header';
-import { Home } from './components/Home/Home';
+import { Root } from './components/Root/Root';
 import { Signup } from './components/Signup/Signup';
 import { Login } from './components/Login/Login';
 import { Profile } from './components/Profile/Profile';
@@ -11,20 +10,23 @@ import { ChangeEmail } from './components/ChangeEmail/ChangeEmail';
 import { ChatBox } from './components/ChatBox/ChatBox';
  
 function App() {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={ <Root/> }>
+      <Route path="sign-up" element={<Signup />}/>
+      <Route path="log-in" element={<Login />}/>
+      <Route path="restore-password" element={<RestorePassword />}/>
+      <Route path="change-email" element={<ChangeEmail />}/>
+      <Route path="profile" element={<Profile />}/>
+      <Route path="chat" element={<ChatBox />} />
+    </Route>
+    ), {
+      basename: process.env.PUBLIC_URL
+    }
+  );
+
   return (
     <AuthProvider>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/sign-up" element={<Signup />}/>
-          <Route path="/log-in" element={<Login />}/>
-          <Route path="/restore-password" element={<RestorePassword />}/>
-          <Route path="/change-email" element={<ChangeEmail />}/>
-          <Route path="/profile" element={<Profile />}/>
-          <Route path="/chat" element={<ChatBox />} />
-        </Routes>
-      </Router>
+      <RouterProvider router={router} />
     </AuthProvider>
   );
 }
